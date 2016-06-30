4309ebe140eb233145a480e43829313451923090
廖鹏辉 <467969@qq.com>
2016-06-30 16:33:03 +0800

    添加混淆,配置存储位置,添加圆角特效,占位图等功能

--- app/src/main/java/com/example/pony/glidedemo/GlideConfiguration.java
+++ app/src/main/java/com/example/pony/glidedemo/GlideConfiguration.java
@@ -8,6 +8,7 @@ import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
 import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.module.GlideModule;
@@ -23,6 +24,8 @@ public class GlideConfiguration implements GlideModule {
         builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
         //设置图片缓存策略  外部存储卡  2M空间  //外部存储空间:默认Android/data/包名/里面
         builder.setDiskCache(new ExternalCacheDiskCacheFactory(context, cacheSize100MegaBytes));
+        //设置为内部存储空间 和大小
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, cacheSize100MegaBytes));
     }
 
     @Override
--- app/src/main/java/com/example/pony/glidedemo/MainActivity.java
+++ app/src/main/java/com/example/pony/glidedemo/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.pony.glidedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.io.File;
 
@@ -20,7 +24,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView iv = (ImageView) findViewById(R.id.iv);
+        //正常使用方法
         Glide.with(this).load("http://xiaoziqianbao.oss-cn-hangzhou.aliyuncs.com/tixianyouhua2.jpg").into(iv);
+        //添加动态图片的方法
         Glide.with(this).load("https://camo.githubusercontent.com/0b993d6b4ae35fa2a7de09a48e7b0d7a4ec79dfa/687474703a2f2f6a636f64656372616565722e636f6d2f75706c6f6164732f32303135303332372f313432373434353336363530333038342e676966").asGif().into(iv);
+        //设置占位图的方法 错误图示  和圆角的方法
+        Glide.with(this).load("http://xiaoziqianbao.oss-cn-hangzhou.aliyuncs.com/tixianyouhua2.jpg").placeholder(getResources().getDrawable(R.mipmap.loading_fail_banner)).error(getResources().getDrawable(R.mipmap.loading_fail_banner)).transform(new GlideRoundTransform(this, 4)).into(iv);
+        //设置Glide的监听
+        Glide.with(this).load("http://xiaoziqianbao.oss-cn-hangzhou.aliyuncs.com/tixianyouhua2.jpg").listener(new RequestListener<String, GlideDrawable>() {
+            @Override
+            public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                Log.e(TAG,"onResourceReady:"+"图片加载成功");
+                //图片加载成功的回调  可以做一些操作  比如显示按钮
+                return false;
+            }
+        }).into(iv);
     }
 }
